07fdfc659dcfaabaf570c4aa2fd3005450a5558c
Michael Peterson <WebDev@ctoms.ca>
2019-04-24 09:50:49 -0600

    addedcsv map data

--- js/data.js
+++ js/data.js
@@ -1,6 +1,74 @@
 variantProductMap = {
 	//torque nut size => size
 	7:91,
+	//Alloy Offset  => size
+	8:91,
 	//apparel color => color
-	9:21
+	9:22,
+	//apparel pant length 2 => apparel pant length
+	11:10,
+	//apparel shirt size => size
+	12:91,
+	//apparel size/length => size
+	13:91,
+	//color 3 => color
+	23:22,
+	//color 5 => color
+	24:22,
+	//color 7 => color
+	25:22,
+	//color H => color
+	26:22,
+	//color TW => color
+	27:22,
+	//crye hat size 7 => size
+	29:91,
+	//dragon size => size
+	31:91,
+	//ECBD matrix size => size
+	34:91,
+	//encompass group colours => color
+	36:22,
+	//dmm size => size
+	37:91,
+	//helmet size => size
+	39:91,
+	//kydex color => color
+	45:22,
+	//mnpa sizes => size
+	55:91,
+	//nexsplint color => color
+	58:22,
+	//pant color => color
+	62:22,
+	//pant leg length => apparel pant length
+	63:10,
+	//pant size/length => size
+	64:91,
+	//peenut color => color
+	69:22,
+	//pouch/soft armour size => size
+	70:91,
+	//shirt size => size
+	74:91,
+	//size 4 => size
+	75:91,
+	//size 6 => size
+	76:91,
+	//size TW => size
+	77:91,
+	//size/length => size
+	78:91,
+	//sizing => size
+	79:91,
+	//sterling cord color => color
+	80:22,
+	//sterling rope sizes 6mm => size
+	81:91,
+	//t-shirt color => color
+	82:91,
+	//torque nut sizes => size
+	83:91,
+	//swivel size => size
+	90:91
 };
